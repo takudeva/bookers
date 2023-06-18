38b9fbfcd1d04c4b3fea9d4d3c49b02f9bbc4ff7
takudeva <126333608+takudeva@users.noreply.github.com>
2023-06-18 00:32:29 +0000

    [Edit]2-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,12 +2,27 @@ class BooksController < ApplicationController
   def new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to book_path(@book.id)
+  end
+
   def index
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
   end
+
+  # ストロングパラメータ
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
